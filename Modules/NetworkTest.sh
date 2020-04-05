0c68251c06aef2ef7eb5b503da2c6d65088312f6
Hyy2001X <1804430051@qq.com>
2020-04-05 04:52:23 -0700

    V2.0,Better UI

--- Modules/NetworkTest.sh
+++ Modules/NetworkTest.sh
@@ -1,43 +1,38 @@
 # AutoBuild Script Module by Hyy2001
 
 function Network_Test() {
-Update=2020.04.02
-Module_Version=V1.0.1
+Update=2020.04.05
+Module_Version=V2.0
+
+function Network_Test_Mod() {
+echo -ne "\r$Blue检测中...$White\r"
+timeout 3 httping -c 1 $Net_URL > /dev/null 2>&1
+if [ $? -eq 0 ];then
+	timeout 3 httping -c 1 $Net_URL > $Home/TEMP/Network
+	Net_IP=`egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $Home/TEMP/Network`
+	echo -e "$Net_URL		$Net_IP		$Yellow正常$White"
+else
+	echo -e "$Net_URL		无法获取		$Red错误$White"
+fi
+}
 
 clear
 Say="Network Test Script $Module_Version by Hyy2001" && Color_B
 echo " "
-Network_OK="$Yellow连接正常$White"
-Network_ERROR="$Red连接错误$White"
+echo -e "$Skyb网址			IP地址			连接状态$White"
 
 Net_URL=www.baidu.com
-timeout 3 httping -c 1 $Net_URL > /dev/null 2>&1
-if [ $? -eq 0 ];then
-	echo -e "$Net_URL		$Network_OK" 
-else
-	echo -e "$Net_URL		$Network_ERROR"
-fi
+Network_Test_Mod
+
 Net_URL=www.gitee.com
-timeout 3 httping -c 1 $Net_URL > /dev/null 2>&1
-if [ $? -eq 0 ];then
-	echo -e "$Net_URL		$Network_OK" 
-else
-	echo -e "$Net_URL		$Network_ERROR"
-fi
+Network_Test_Mod
+
 Net_URL=www.github.com
-timeout 3 httping -c 1 www.github.com > /dev/null 2>&1
-if [ $? -eq 0 ];then
-	echo -e "$Net_URL		$Network_OK" 
-else
-	echo -e "$Net_URL		$Network_ERROR"
-fi
+Network_Test_Mod
+
 Net_URL=www.google.com
-timeout 3 httping -c 1 www.google.com > /dev/null 2>&1
-if [ $? -eq 0 ];then
-	echo -e "$Net_URL		$Network_OK" 
-else
-	echo -e "$Net_URL		$Network_ERROR"
-fi
+Network_Test_Mod
+
 echo ""
 Enter	
 }
