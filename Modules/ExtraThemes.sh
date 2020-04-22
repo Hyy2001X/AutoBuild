ae944e144bf8215519fdb6a8815c7ea2b91f788d
Hyy2001X <1804430051@qq.com>
2020-04-22 18:03:44 +0800

    Version bump to "V2.1-DEV"

--- Modules/ExtraThemes.sh
+++ Modules/ExtraThemes.sh
@@ -1,8 +1,8 @@
 # AutoBuild Script Module by Hyy2001
 
 function ExtraThemes() {
-Update=2020.04.20
-Module_Version=V2.0-DEV
+Update=2020.04.22
+Module_Version=V2.1-DEV
 PKGHome=$Home/Projects/$Project/package
 
 ExtraThemes_info() {
@@ -24,14 +24,11 @@ Enter
 
 
 ExtraThemes_mod_git() {
-cd theme
 if [ -d ./$PKG_NAME ];then
 	rm -rf ./$PKG_NAME
 fi
-echo " "
-git clone $PKG_URL $PKG_NAME
-echo " "
-if [ -f ./$PKG_NAME/Makefile ];then
+git clone $PKG_URL $PKG_NAME > /dev/null 2>&1
+if [ -f ./$PKG_NAME/Makefile ] || [ -f ./$PKG_NAME/README.md ];then
 	Say="$PKG_NAME 添加成功!" && Color_Y
 else
 	Say="$PKG_NAME 添加失败!" && Color_R
@@ -40,29 +37,26 @@ sleep 3
 }
 
 ExtraThemes_mod_svn() {
-cd theme
 if [ -d ./$PKG_NAME ];then
 	rm -rf ./$PKG_NAME
 fi
 svn checkout $PKG_URL $PKG_NAME > /dev/null 2>&1
-echo " "
 if [ -f ./$PKG_NAME/Makefile ] || [ -f ./$PKG_NAME/README.md ];then
 	Say="$PKG_NAME 添加成功!" && Color_Y
 	rm -rf ./$PKG_NAME/.svn
 else
 	Say="$PKG_NAME 添加失败!" && Color_R
 fi
-sleep 3
+sleep 2
 }
 
 while :
 do
 	cd $Home/Projects/$Project/package
-	if [ -d ./theme ];then
-		:
-	else
+	if [ ! -d ./theme ];then
 		mkdir theme
 	fi
+	cd theme
 	clear
 	echo -e "${Blue}Extra Themes Script $Module_Version by Hyy2001${White}"
 	echo -e "$Skyb"
@@ -80,16 +74,83 @@ do
 	echo "12.luci-theme-Butterfly-dark"
 	echo "13.luci-theme-netgearv2"
 	echo -e "${White}"
+	echo "a.添加所有主题"
 	echo "x.关于"
 	echo "q.返回"
 	echo " "
 	read -p '请从上方选择一个主题包:' Choose
+	echo " "
 	case $Choose in
 	q)
 		break
 	;;
 	x)
 		ExtraThemes_info
+	;;
+	a)
+		PKG_NAME=luci-theme-argon
+		if [ $Project == Lede ];then
+			cd $PKGHome
+			if [ -f ./lean/luci-theme-argon ];then
+				rm -rf ./lean/luci-theme-argon
+			else
+				:
+			fi
+			PKG_URL=" -b 18.06 https://github.com/jerrykuku/luci-theme-argon"
+			ExtraThemes_mod_git
+			mv $PKGHome/theme/luci-theme-argon $PKGHome/lean/$PKG_NAME
+		else
+			PKG_URL=" -b 19.07_stable https://github.com/jerrykuku/luci-theme-argon"
+			ExtraThemes_mod_git
+		fi
+
+		PKG_NAME=luci-theme-argon-mc
+		PKG_URL=https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/$PKG_NAME
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-argon-dark-mod
+		PKG_URL=https://github.com/Lienol/openwrt-package/trunk/lienol/$PKG_NAME
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-argon-light-mod
+		PKG_URL=https://github.com/Lienol/openwrt-package/trunk/lienol/$PKG_NAME
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-bootstrap-mod
+		PKG_URL=https://github.com/Lienol/openwrt-package/trunk/lienol/$PKG_NAME
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-rosy
+		PKG_URL=https://github.com/rosywrt/$PKG_NAME/trunk/
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-atmaterial
+		PKG_URL=https://github.com/openwrt-develop/$PKG_NAME
+		ExtraThemes_mod_git
+
+		PKG_NAME=luci-theme-darkmatter
+		PKG_URL=https://github.com/Lienol/$PKG_NAME/trunk/
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-opentomcat
+		PKG_URL=https://github.com/Leo-Jo-My/$PKG_NAME
+		ExtraThemes_mod_git
+
+		PKG_NAME=luci-theme-opentomato
+		PKG_URL=https://github.com/Leo-Jo-My/$PKG_NAME
+		ExtraThemes_mod_git
+
+		PKG_NAME=luci-theme-Butterfly
+		PKG_URL=https://github.com/Leo-Jo-My/$PKG_NAME
+		ExtraThemes_mod_git
+
+		PKG_NAME=luci-theme-Butterfly-dark
+		PKG_URL=https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/$PKG_NAME
+		ExtraThemes_mod_svn
+
+		PKG_NAME=luci-theme-netgearv2
+		PKG_URL=https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/$PKG_NAME
+		ExtraThemes_mod_svn
 	;;
 	1)
 		PKG_NAME=luci-theme-argon
@@ -103,7 +164,7 @@ do
 			ExtraThemes_mod_git
 			mv $PKGHome/theme/luci-theme-argon $PKGHome/lean/$PKG_NAME
 		else
-			PKG_URL=https://github.com/jerrykuku/$PKG_NAME
+			PKG_URL=" -b 19.07_stable https://github.com/jerrykuku/luci-theme-argon"
 			ExtraThemes_mod_git
 		fi
 	;;
