25be5220935060465d71064bf3da487b6b55646e
Hyy2001X <1804430051@qq.com>
2020-07-09 00:13:29 +0800

    V2.0-BETA 重构代码

--- Modules/StorageStat.sh
+++ Modules/StorageStat.sh
@@ -1,87 +1,52 @@
 # AutoBuild Script Module by Hyy2001
 
-function StorageStat() {
-Update=2020.06.23
-Module_Version=V1.2
+StorageStat() {
+Update=2020.07.09
+Module_Version=V2.0-BETA
 
 clear
 Say="Loading Configuration..." && Color_Y
+echo " "
 cd $Home/Projects
-if [ -d ./Lede ];then
-	Lede_Size=$((`du -s Lede |awk '{print $1}'`))
-	Lede_Check=1
-else
-	Lede_Check=0
-fi
-if [ -d ./Openwrt ];then
-	Openwrt_Size=$((`du -s Openwrt |awk '{print $1}'`))
-	Openwrt_Check=1
-else
-	Openwrt_Check=0
-fi
-if [ -d ./Lienol ];then
-	Lienol_Size=$((`du -s Lienol |awk '{print $1}'`))	
-	Lienol_Check=1
-else
-	Lienol_Check=0
-fi
+Lede_Size=`du -sh Lede | awk '{print $1}'`
+Openwrt_Size=`du -sh Openwrt | awk '{print $1}'`
+Lienol_Size=`du -sh Lienol | awk '{print $1}'`
 cd $Home
-if [ -d ./Backups ];then
-	Backups_Size=$((`du -s Backups |awk '{print $1}'`))
-	Backups_Check=1
-else
-	Backups_Check=0
-fi
-if [ -d ./Packages ];then
-	Packages_Size=$((`du -s Packages |awk '{print $1}'`))
-	Packages_Check=1
-else
-	Packages_Check=0
-fi
-
+Backups_Size=`du -sh Backups | awk '{print $1}'`
+Packages_Size=`du -sh Packages | awk '{print $1}'`
 clear
-Say="Storage Statistics $Module_Version" && Color_B
+Say="Storage Statistics Script $Module_Version" && Color_B
 Decoration
 echo -e "$Skyb项目名称	存储位置				存储占用$White"
 echo " "
-if [ $Lede_Check == 1 ];then
-	echo -ne "${Yellow}"
-	awk 'BEGIN{printf "Lede		./Projects/Lede				%.2fGB\n",'$((Lede_Size))'/1048576}'
-	echo -ne "${White}"
+if [ ! $Lede_Size == 0 ];then
+	Say="Lede		/Projects/Lede				$Lede_Size" && Color_Y
 else
-	echo -e "${Red}Lede		未检测到				0KB${White}"
+	Say="Lede		未检测到				0KB" && Color_R
 fi
 
-if [ $Openwrt_Check == 1 ];then
-	echo -ne "${Yellow}"
-	awk 'BEGIN{printf "Openwrt		./Projects/Openwrt			%.2fGB\n",'$((Openwrt_Size))'/1048576}'
-	echo -ne "${White}"
+if [ ! $Openwrt_Size == 0 ];then
+	Say="Openwrt		/Projects/Openwrt			$Openwrt_Size" && Color_Y
 else
-	echo -e "${Red}Openwrt		未检测到				0KB${White}"
+	Say="Openwrt		未检测到				0KB" && Color_R
 fi
 
-if [ $Lienol_Check == 1 ];then
-	echo -ne "${Yellow}"
-	awk 'BEGIN{printf "Lienol		./Projects/Lienol			%.2fGB\n",'$((Lienol_Size))'/1048576}'
-	echo -ne "${White}"
+if [ ! $Lienol_Size == 0 ];then
+	Say="Lienol		/Projects/Lienol			$Lienol_Size" && Color_Y
 else
-	echo -e "${Red}Lienol		未检测到				0KB${White}"
+	Say="Lienol		未检测到				0KB" && Color_R
 fi
 echo " "
-if [ $Backups_Check == 1 ];then
-	echo -ne "${Yellow}"
-	awk 'BEGIN{printf "备份		./Backups				%.2fMB\n",'$((Backups_Size))'/1024}'
-	echo -ne "${White}"
+if [ ! $Backups_Size == 0 ];then
+	Say="备份		/Backups				$Backups_Size" && Color_Y
 else
-	echo -e "${Red}备份		未检测到				0KB${White}"
+	Say="备份		未检测到				0KB" && Color_R
 fi
 
-if [ $Packages_Check == 1 ];then
-	echo -ne "${Yellow}"
-	awk 'BEGIN{printf "生成的固件	./Packages				%.2fMB\n",'$((Packages_Size))'/1024}'
-	echo -ne "${White}"
+if [ ! $Packages_Size == 0 ];then
+	Say="生成的固件	/Packages				$Packages_Size" && Color_Y
 else
-	echo -e "${Red}生成的固件	未检测到				0KB${White}"
+	Say="生成的固件	未检测到				0KB" && Color_R
 fi
 Decoration
 echo " "
