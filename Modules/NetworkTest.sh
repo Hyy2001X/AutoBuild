1988c9090aad8ca3af0ee2c7709a63d2197e53e8
Hyy2001X <1804430051@qq.com>
2020-07-16 12:21:59 +0800

    V2.6.0 优化测试代码

--- Modules/NetworkTest.sh
+++ Modules/NetworkTest.sh
@@ -1,22 +1,8 @@
 # AutoBuild Script Module by Hyy2001
 
-function Network_Test() {
-Update=2020.06.28
-Module_Version=V2.5.1
-
-function Network_Test_Mod() {
-echo -ne "\r$Blue检测中...$White\r"
-timeout 3 httping -c 1 $Net_URL > /dev/null 2>&1
-if [ $? -eq 0 ];then
-	timeout 3 httping -c 1 $Net_URL > $Home/TEMP/Network
-	Net_IP=`egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $Home/TEMP/Network`
-	egrep -o "/[0-9]+\.[0-9]+\/" $Home/TEMP/Network > $Home/TEMP/Network_PING
-	Net_PING=`egrep -o "[0-9]+\.[0-9]+" $Home/TEMP/Network_PING`
-	echo -e "$Net_URL		$Net_IP		$Yellow正常$White 		$Net_PING"
-else
-	echo -e "$Net_URL		$Red无法获取		错误		错误$White"
-fi
-}
+Network_Test() {
+Update=2020.07.16
+Module_Version=V2.6.0
 
 clear
 Say="Network Test Script $Module_Version" && Color_B
@@ -24,23 +10,26 @@ Decoration
 echo -e "$Skyb网址			IP地址			状态		延迟$White"
 echo " "
 
-Net_URL=www.baidu.com
-Network_Test_Mod
+Network_Mod www.baidu.com
+Network_Mod www.github.com
+Network_Mod www.google.com
+Network_Mod www.github.io
+Network_Mod dl.google.com
 
-Net_URL=www.github.com
-Network_Test_Mod
+echo " " && Decoration && echo " "
+Enter
+}
 
-Net_URL=www.google.com
-Network_Test_Mod
-
-Net_URL=www.github.io
-Network_Test_Mod
-
-Net_URL=dl.google.com
-Network_Test_Mod
-
-echo " "
-Decoration
-echo " "
-Enter	
+Network_Mod() {
+echo -ne "\r$Blue检测中...$White\r"
+timeout 3 httping -c 1 $1 > /dev/null 2>&1
+if [ $? -eq 0 ];then
+	timeout 3 httping -c 1 $1 > $Home/TEMP/Network
+	Net_IP=`egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $Home/TEMP/Network`
+	egrep -o "/[0-9]+\.[0-9]+\/" $Home/TEMP/Network > $Home/TEMP/Network_PING
+	Net_PING=`egrep -o "[0-9]+\.[0-9]+" $Home/TEMP/Network_PING`
+	echo -e "$1		$Net_IP		$Yellow正常$White 		$Net_PING"
+else
+	echo -e "$1		$Red无法获取		错误		错误$White"
+fi
 }
