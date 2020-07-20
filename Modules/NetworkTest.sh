23e09d0ed51afc3881b0c2175f722ee301f4bf20
Hyy2001X <1804430051@qq.com>
2020-07-20 12:46:42 +0800

    V2.7 优化测试代码 移除测试项

--- Modules/NetworkTest.sh
+++ Modules/NetworkTest.sh
@@ -1,20 +1,17 @@
 # AutoBuild Script Module by Hyy2001
 
 Network_Test() {
-Update=2020.07.16
-Module_Version=V2.6.0
+Update=2020.07.20
+Module_Version=V2.7
 
 clear
 Say="Network Test Script $Module_Version" && Color_B
 Decoration
 echo -e "$Skyb网址			IP地址			状态		延迟$White"
 echo " "
-
-Network_Mod www.baidu.com
-Network_Mod www.github.com
-Network_Mod www.google.com
-Network_Mod www.github.io
-Network_Mod dl.google.com
+Network_Mod www.baidu.com 1
+Network_Mod www.github.com 2
+Network_Mod www.google.com 2
 
 echo " " && Decoration && echo " "
 Enter
@@ -22,12 +19,11 @@ Enter
 
 Network_Mod() {
 echo -ne "\r$Blue检测中...$White\r"
-timeout 3 httping -c 1 $1 > /dev/null 2>&1
+timeout 3 httping -c 1  $1 > /dev/null 2>&1
 if [ $? -eq 0 ];then
-	timeout 3 httping -c 1 $1 > $Home/TEMP/Network
-	Net_IP=`egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $Home/TEMP/Network`
-	egrep -o "/[0-9]+\.[0-9]+\/" $Home/TEMP/Network > $Home/TEMP/Network_PING
-	Net_PING=`egrep -o "[0-9]+\.[0-9]+" $Home/TEMP/Network_PING`
+	timeout $(($2*3)) httping -c $2 $1 > $Home/TEMP/Network_Test
+	Net_IP=`awk 'NR==2' $Home/TEMP/Network_Test | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"`
+	Net_PING=`egrep -o "/[0-9]+\.[0-9]+\/" $Home/TEMP/Network_Test | egrep -o "[0-9]+\.[0-9]+"`
 	echo -e "$1		$Net_IP		$Yellow正常$White 		$Net_PING"
 else
 	echo -e "$1		$Red无法获取		错误		错误$White"
