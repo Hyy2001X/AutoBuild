68e646c53cc05528ebf4713097b5ca6f27abe903
Nxizz <69080970+Nxiz@users.noreply.github.com>
2020-08-13 14:23:21 +0800

    V3.8.9-b 修改 AutoBuild脚本更新方式

--- AutoBuild.sh
+++ AutoBuild.sh
@@ -3,8 +3,8 @@
 # Author	Hyy2001、Nxiz
 # Github	https://github.com/Hyy2001X/AutoBuild
 # Supported System:Ubuntu 20.04、Ubuntu 19.10、Ubuntu 18.04、Deepin 20
-Update=2020.08.03
-Version=V3.8.8
+Update=2020.08.13
+Version=V3.8.9-b
 
 Second_Menu() {
 while :
@@ -597,40 +597,39 @@ done
 Script_Update() {
 timeout 3 ping -c 1 www.baidu.com > /dev/null 2>&1
 if [ $? -eq 0 ];then
-	cd $Home
 	clear
-	Say="开始下载更新..." && Color_Y
-	echo " "
-	Old_Version=`awk 'NR==7' $Home/AutoBuild.sh | awk -F'[="]+' '/Version/{print $2}'`
-	Backups_Dir=$Home/Backups/OldVersion/AutoBuild-Core-$Old_Version
-	if [ -d $Backups_Dir ];then
-		rm -rf $Backups_Dir
-	fi
-	mkdir -p $Backups_Dir
-	cp $Home/AutoBuild.sh $Backups_Dir/AutoBuild.sh
-	cp $Home/README.md $Backups_Dir/README.md
-	cp $Home/LICENSE $Backups_Dir/LICENSE
-	cp -a $Home/Additional $Backups_Dir/Additional
-	cp -a $Home/Modules $Backups_Dir/Modules
-	rm -rf ./TEMP
-	svn checkout https://github.com/Hyy2001X/AutoBuild/trunk ./TEMP
-	echo " "
-	if [ -f ./TEMP/AutoBuild.sh ];then
-		rm -rf ./Modules
-		rm -rf ./Additional
-		rm -f ./AutoBuild.sh
-		mv ./TEMP/* $Home
-		chmod +x $Home/AutoBuild.sh
-		chmod +x -R $Home/Modules
-		rm -rf ./TEMP
-		New_Version=`awk 'NR==7' $Home/AutoBuild.sh | awk -F'[="]+' '/Version/{print $2}'`
-		echo -e "${Yellow}AutoBuild_Core ${Blue}$Old_Version --> $New_Version${Yellow}"
-		echo " "
-		read -p "AutoBuild 更新成功!" Key
-		./AutoBuild.sh
+	Say="正在下载更新...\n" && Color_Y
+	cd $Home/Backups
+	if [ "`ls -A ./AutoBuild-Update`" = "" ];then
+		git clone https://github.com/Hyy2001X/AutoBuild AutoBuild-Update
 	else
-		Say="AutoBuild 更新失败!" && Color_R
-		sleep 2
+		cd ./AutoBuild-Update
+		Update_Logfile=$Home/Log/Script_Update_`(date +%Y%m%d_%H:%M)`.log
+		git pull 2>&1 | tee $Update_Logfile
+		if [ $(grep -o "fatal: 无法访问" $Update_Logfile | wc -l) = "0" ];then
+			if [ $(grep -o "已经是最新的" $Update_Logfile | wc -l) = "1" ];then
+				Say="\n强制合并到本地文件..." && Color_Y
+			else
+				Say="\n合并到本地文件..." && Color_Y
+			fi
+			Old_Version=`awk 'NR==7' $Home/AutoBuild.sh | awk -F'[="]+' '/Version/{print $2}'`
+			Old_Version_Dir=$Old_Version-`(date +%Y%m%d_%H:%M)`
+			Backups_Dir=$Home/Backups/OldVersion/AutoBuild-Core-$Old_Version_Dir
+			mkdir $Backups_Dir
+			mv $Home/AutoBuild.sh $Backups_Dir/AutoBuild.sh
+			mv $Home/README.md $Backups_Dir/README.md
+			mv $Home/LICENSE $Backups_Dir/LICENSE
+			mv $Home/Additional $Backups_Dir/Additional
+			mv $Home/Modules $Backups_Dir/Modules
+			cp -a * $Home
+			Say="\nAutoBuild 已自动备份到'/Backups/OldVersion/AutoBuild-Core-$Old_Version_Dir'" && Color_B
+			echo -e "$Yellow"
+			read -p "AutoBuild 更新成功!" Key
+			$Home/AutoBuild.sh
+		else
+			echo -e "$Red"
+			read -p "AutoBuild 更新失败!" Key
+		fi
 	fi
 else
 	Say="\n网络连接错误,更新失败!" && Color_R
